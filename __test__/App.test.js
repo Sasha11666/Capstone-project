758e1182f8970be836bae0646d5afbb475bb1cc3
Sasha11666 <basovasasa087@gmail.com>
2023-06-20 10:33:05 +0300

    added tests and form validation

--- __test__/App.test.js
+++ __test__/App.test.js
@@ -4,11 +4,18 @@ import { render, screen } from "@testing-library/react";
 import Form from "../src/components/Form";
 import { initializeTimes } from "../src/components/Form";
 import { reducer } from "../src/components/Form";
+import { highltghtField } from "../src/components/Form";
 
 test("Renders the Form label", () => {
   render(<Form />);
   const labelElement = screen.getByText("Your name");
   expect(labelElement).toBeInTheDocument();
+});
+
+test("input guests should have min attribute", () => {
+  render(<Form />);
+  const inputGuests = screen.getByTestId("guests");
+  expect(inputGuests.getAttribute("min")).toBe("1");
 });
 
 test("should test that initializeTimes returns an array", () => {
@@ -22,4 +29,18 @@ test("should test that reducer returns a an array", () => {
   let state = ["17:00", "18:00", "19:00", "20:00", "21:00", "22:00"];
   let action = "2023-06-23";
   expect(reducer(state, action)).toBeInstanceOf(Array);
+});
+
+test("should test that name field is highlighted if the value is empty", () => {
+  render(<Form />);
+  highltghtField("", "input-name");
+  const selectedField = screen.getByTestId("input-name");
+  expect(selectedField.classList).toMatchObject({ 0: "highlighted" });
+});
+
+test("should test that name field is not highlighted if the value is not empty", () => {
+  render(<Form />);
+  highltghtField("Ellie", "input-name");
+  const selectedField = screen.getByTestId("input-name");
+  expect(selectedField.classList).toMatchObject({});
 });
